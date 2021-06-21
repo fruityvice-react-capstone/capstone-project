3b9992ac0eebb4e60bb2bba8fc5d7e01de8b61e5
Daniel de la Huerta <ddelahuerta@gmail.com>
2021-06-21 15:03:43 -0400

    new repo file with Rick and Morty

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: "blah" };
+    this.testNav = this.testNav.bind(this);
+  }
+  testNav() {
+    this.props.history.push("/about");
+    console.log("this is being clicked");
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              {/* <Link to="/about">About</Link> */}
+              <button onClick={this.testNav}>Characters</button>
+            </li>
+            <li>
+              <Link to="/fruitapp">Fruit App</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
   }
 }
 
-export default Navigation;
+export default withRouter(Navigation);
