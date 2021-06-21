958bf3fd3aef11ad602c6bdd543ff4ec57c12ab6
JoycesCodes <jjyoung145@gmail.com>
2021-06-21 15:17:51 -0400

    changed imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
-import About from "./components/Characters/Characters";
-import FruitInfo from "./components/Search/Search";
+import Characters from "./components/Characters/Characters";
+import Search from "./components/Search/Search";
 
 import { Switch, Route } from "react-router-dom";
 
@@ -18,10 +18,10 @@ function App() {
           <Home />
         </Route>
         <Route exact path="/about">
-          <About />
+          <Characters />
         </Route>
         <Route exact path="/fruitInfo">
-          <FruitInfo />
+          <Search />
         </Route>
       </Switch>
     </div>
