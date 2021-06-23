7e5cc820f1a591908280d4a9cddd783c32018336
Joyce Young <jjyoung145@gmail.com>
2021-06-23 10:24:49 -0400

    helped Johanny link character details page and fixed minor routing errors:

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -7,15 +7,15 @@ class Characters extends Component {
       characters: [],
     };
   }
-  // componentDidMount() {
-  //   // const selectedId;
-  //   fetch(`https://rickandmortyapi.com/api/character/${selectedId}`);
-  // }
+  componentDidMount() {
+    const selectedId = this.props.match.params.selectedId;
+    fetch(`https://rickandmortyapi.com/api/character/${selectedId}`);
+  }
   render() {
     return (
       <div>
         <h2>Name</h2>
-        <img></img>
+        <img src></img>
         <strong>Species: </strong>
         <strong>Status: </strong>
         <strong>Gender: </strong>
