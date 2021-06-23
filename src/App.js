1957eb108077fa35a8dc956649d9d0e035c75224
Joyce Young <jjyoung145@gmail.com>
2021-06-23 11:16:55 -0400

    completed the random button

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
 import Characters from "./components/Characters/Characters";
 import Search from "./components/Search/Search";
-import Random from "./components/Random/Random";
+// import Random from "./components/Random/Random";
 import CharacterList from "./components/CharacterList/CharacterList";
 
 import { Switch, Route } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.css";
 <link
   rel="stylesheet"
@@ -28,22 +29,21 @@ function App() {
       </div>
       <hr></hr>
       <Switch>
-        {
-          <Route exact path="/">
-            <Home />
-          </Route>
-        }
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route exact path="/characters">
           <CharacterList />
         </Route>
-        <Route path="/character/:selectedId" component={Characters} />
-
+        <Route path="/characters/:selectedId" component={Characters} />
         <Route path="/Search">
           <Search />
         </Route>
-        <Route path="/Random">
+
+        {/* no longer needed using materials-ui
+        <Route path="/random">
           <Random />
-        </Route>
+        </Route> */}
       </Switch>
     </div>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 import { withRouter } from "react-router";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Form, FormControl } from "react-bootstrap";
 
 class Navigation extends Component {
+  randomNumber = () => {
+    const min = 1;
+    const max = 671;
+    return (Math.random() * (max - min) + min).toFixed(0);
+  };
   render() {
     return (
       <div>
         <Container fluid>
           <Row>
             <Col>
-              {" "}
-              <Link to="/">Home</Link>{" "}
+              <Link to="/">Home</Link>
             </Col>
             <Col>
               <Link to="/characters">Characters</Link>
             </Col>
             <Col>
-              {" "}
-              <Link to="/search">Search</Link>{" "}
+              <Link to="/search">Search</Link>
+            </Col>
+            <Col>
+              <Button component={Link} to={`/characters/${this.randomNumber}`}>
+                Random Character
+              </Button>
             </Col>
           </Row>
         </Container>
